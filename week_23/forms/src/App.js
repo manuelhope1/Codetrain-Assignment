5d2204a319697e2fc20e70269132a084fa3c2dad
Emmanuel Hope Mensah <manuelhopemensah@gmail.com>
2023-04-24 11:31:08 -0500

    23 added

---
+++ week_23/forms/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ClassForm from './components/react-class-form';
+
+function App() {
+  return (
+    <div id="body">
+      <ClassForm/>
+    </div>
+  );
+}
+
+export default App;
